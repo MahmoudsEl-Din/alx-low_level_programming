52a2db0a0d8f2f838e7b259dc890669cd9bbbe5b
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2023-03-22 04:04:07 +0200

    Add files via upload

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "0x02-functions_nested_loops.h"
+/**
+ * main - Entry point function
+ *
+ * Description: Prints _putchar using a library followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
