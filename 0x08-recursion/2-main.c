fe471f6ef851d528ce67fadb2fa9cd819479a6b1
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2023-04-06 04:45:32 +0200

    Add files via upload

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code for alx.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
