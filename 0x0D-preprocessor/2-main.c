16c5a9a780a42c386d0a8f74ae140f2c683a71ac
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2023-04-18 02:13:42 +0200

    Add files via upload

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *res;
+
+	res = __FILE__;
+	printf("%s\n", res);
+	return (0);
+}
