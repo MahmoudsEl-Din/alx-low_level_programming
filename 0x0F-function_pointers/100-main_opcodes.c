4809f18bee714c4ac1ac7b0fb1ec2b581a71940d
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2023-04-20 01:43:20 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <udis86.h>
 
 /**
- * int_index -  function that searches for an integer.
- * @size: is the number of elements in the array array
- * @array: an array
- * @cmp: is a pointer to the function to be used to compare values
- *
- * Return: returns the index of the first element for which the cmp
- * function does not return 0
- */
-
-int int_index(int *array, int size, int (*cmp)(int))
+  * main - Entry point
+  *
+  * @argc: Argument count
+  * @argv: Argument variable
+  *
+  * Return: Always return 0
+  */
+int main(int argc, char *argv[])
 {
+	ud_t ud_obj;
+	int val = 0, i = 0;
+
+	if (argc == 2)
+	{
+		val = atoi(argv[1]);
+
+		if (val < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+		ud_unit(&ud_obj);
+		ud_set_input_buffer(&ud_obj, argv[1], val);
+		ud_set_mode(&ud_obj, 64);
+		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+		while (ud_disassemble(&ud_obj))
+		{
+			printf("\t%s\n", ud_insn_hex(&ud_obj));
+		}
+	}
+
+	return (0);
 }
