75765f554af440758e56738f7cd6e94a37092340
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2023-04-29 17:40:24 +0200

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,14 +8,7 @@
  *
  * Return: The number of nodes in the linked list.
  */
-#include "lists.h"
 
-/**
- * free_listp - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
 void free_listp(listp_t **head)
 {
 	listp_t *temp;
